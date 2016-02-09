490a2f99bac1a8afdc0c4285f4639bb9e685a53e
EManushkin <e.manushkin@gmail.com>
2016-02-09 18:12:34 +0200

    добавил класс для создания вертикальной линии

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,28 +11,13 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            //p1.Draw();
+            p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-            //p2.Draw();
+            p2.Draw();
 
-            Point p3 = new Point(7, 9, '&');
-            //p3.Draw();
-
-            Point p4 = new Point(3, 2, '%');
-            //p4.Draw();
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-
-            foreach (Point i in pList)
-            {
-                i.Draw();
-            }
-
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
 
             Console.ReadLine();
         }
